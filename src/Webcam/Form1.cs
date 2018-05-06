7673dc2ac4c0d7725e6b1bc9e94738cf344c0a61
cbovar <cedric.bovar@gmail.com>
2018-05-06 13:07:35 +0800

    GPU + run model asynchronously

--- src/Webcam/Form1.cs
+++ src/Webcam/Form1.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using AForge.Video;
 using AForge.Video.DirectShow;
@@ -17,19 +21,24 @@ namespace Webcam
     /// </summary>
     public partial class Form1 : Form
     {
-        public const int IMAGE_WIDTH = 64; // Must be the same as when training model
-        public const int IMAGE_HEIGHT = 64;
+        public const int IMAGE_WIDTH = 256; // Must be the same as when training model
+        public const int IMAGE_HEIGHT = 256;
 
         private FilterInfoCollection _devices;
         private VideoCaptureDevice _frame;
         private string _output;
-        private readonly Op<float> _yhat;
+        private Op<float> _yhat;
 
         public Form1()
         {
             InitializeComponent();
 
-            this._yhat = ConvNetSharp.Flow.Serialization.SerializationExtensions.Load<float>("FaceDetection", false)[0];
+            var currentDirectory = Directory.GetCurrentDirectory();
+            this.textBox.Text = currentDirectory;
+            this.folderBrowserDialog1.SelectedPath = currentDirectory;
+            this._output = currentDirectory;
+
+            Task.Run(() => StartAI());
         }
 
         private void browseButton_Click(object sender, EventArgs e)
@@ -41,7 +50,7 @@ namespace Webcam
 
         private void takeImageButton_Click(object sender, EventArgs e)
         {
-            if (this._output != "")
+            if (!string.IsNullOrEmpty(this._output))
             {
                 this.pictureBox.Image?.Save(this._output + "\\Image.png");
             }
@@ -60,33 +69,77 @@ namespace Webcam
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
-            this._frame.Stop();
+            this._frame?.Stop();
         }
 
         private void NewFrame_event(object send, NewFrameEventArgs e)
         {
             try
             {
-                var data = new float[IMAGE_WIDTH * IMAGE_HEIGHT];
-                BitmapTool.ExtractData(IMAGE_WIDTH, IMAGE_HEIGHT, data, e.Frame);
+                var webcamFrame = e.Frame;
 
-                var input = BuilderInstance.Volume.From(data, new Shape(IMAGE_WIDTH, IMAGE_HEIGHT, 1, 1));
+                //webcamFrame = (Bitmap)Image.FromFile(@"C:\Pro\Github\FaceLocalisation\Face\Dataset\helen_5\2947090433_1.jpg");
 
-                using (var session = new Session<float>())
+                if (this.queueIn.Count == 0)
                 {
-                    var dico = new Dictionary<string, Volume<float>> {{"x", input}};
-                    var result = session.Run(this._yhat, dico);
-
-                    BitmapTool.DrawBoundingBox(e.Frame, new BoundingBox { x1 = result.Get(0), y1 = result.Get(1), x2 = result.Get(2), y2 = result.Get(3) }, Color.Blue);
+                    this.queueIn.Enqueue((Bitmap)webcamFrame.Clone());
                 }
 
-                this.pictureBox.Image = (Image)e.Frame.Clone();
+                if (this.queueOut.TryDequeue(out var boundingBox))
+                {
+                    this.last_boundingBox = boundingBox;
+                }
 
+                if (this.last_boundingBox != null)
+                {
+                    BitmapTool.DrawBoundingBox(webcamFrame, this.last_boundingBox, Color.Blue);
+                }
+
+                this.pictureBox.Image = (Image)webcamFrame.Clone();
             }
             catch (Exception ex)
             {
             }
         }
+
+        private void StartAI()
+        {
+            BuilderInstance<float>.Volume = new VolumeBuilder(); // Needed for GPU, must be done on AI thread
+
+            // Must be done after setting BuilderInstance<float>.Volume
+            this._yhat = ConvNetSharp.Flow.Serialization.SerializationExtensions.Load<float>("FaceDetection", false)[0];
+
+            var data = new float[IMAGE_WIDTH * IMAGE_HEIGHT];
+            var stopwatch = new Stopwatch();
+
+            while (true)
+            {
+                if (this.queueIn.TryDequeue(out var bitmap))
+                {
+                    BitmapTool.ExtractData(IMAGE_WIDTH, IMAGE_HEIGHT, data, bitmap);
+                    var input = BuilderInstance.Volume.From(data, new Shape(IMAGE_WIDTH, IMAGE_HEIGHT, 1, 1));
+
+                    using (var session = new Session<float>())
+                    {
+                        var dico = new Dictionary<string, Volume<float>> { { "x", input } };
+
+                        stopwatch.Restart();
+                        var result = session.Run(this._yhat, dico);
+                        stopwatch.Stop();
+                        Debug.WriteLine(stopwatch.ElapsedMilliseconds);
+
+                        //var boundingBox = new BoundingBox { x1 =0.2f + DateTime.Now.Second / 60.0f * 0.5f, y1 = 0.2f, x2 = 0.6f, y2 = 0.6f };
+                        var boundingBox = new BoundingBox { x1 = result.Get(0), y1 = result.Get(1), x2 = result.Get(2), y2 = result.Get(3) };
+                        this.queueOut.Enqueue(boundingBox);
+                    }
+                }
+
+            }
+        }
+
+        readonly ConcurrentQueue<Bitmap> queueIn = new ConcurrentQueue<Bitmap>();
+        readonly ConcurrentQueue<BoundingBox> queueOut = new ConcurrentQueue<BoundingBox>();
+        private BoundingBox last_boundingBox;
 
         private void Start_cam()
         {
